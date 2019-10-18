ab17b824dfd38c21af0c486b5031ed12d717121d
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2019-10-18 18:46:46 -0300

    Optimized the carousel and fixed the bulma html problem

---
+++ src/components/Map/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.css'
+
+const Map = ({ className }) => {
+  return (
+    <div className={className}>
+
+    </div>
+  )
+}
+
+export default Map;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import Header from '../components/Header'
 import About  from '../components/About'
 import Specialities from '../components/Specialties'
+import Map from '../components/Map'
 
 import './styles.css'
 
 const App = () => {
   return (
     <div className="content">
+      <Helmet>
+        <title>Adriana Miguel</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"/>
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+      </Helmet>
+
       <Header className="header" id="inicio"></Header>
       <About  className="about" id="sobre"></About>
       <Specialities className="specs" id="especialidades"></Specialities>
+      <Map className="google-map"></Map>
     </div>
   );
 }
