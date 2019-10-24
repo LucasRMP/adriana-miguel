53fe49302825216a29793faa95e7c77bf8a2fdab
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2019-10-24 13:41:21 -0300

    Contact form working with only one redirect

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import logo from './assets/white-logo.svg'
 import facebookLogo from './assets/facebook.svg'
@@ -7,30 +7,89 @@ import whatsappLogo from './assets/whatsapp.svg'
 import './styles.css'
 
 const Contact = ({ className, id }) => {
-  return (
-    <>
-      <div className={className} id={id}>
-        <img className="logo" src={logo} />
-        <div className="social-media-icons">
-          <a href="https://www.facebook.com/draadrianamiguel/" className="social-media-link">
-            <img className="social-media-icon" src={facebookLogo}/>
-          </a>
-          <a href="https://www.instagram.com/draadrianamiguel/" className="social-media-link">
-            <img className="social-media-icon" src={instagramLogo}/>
-          </a>
-          <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5562981856722&text=Oi!" className="social-media-link">
-            <img className="social-media-icon" src={whatsappLogo}/>
-          </a>
-        </div>
-        <div className="contacts">
-          <p className="phones">(62) 3928-0988<br/>(62) 98185-6722</p>
-          <p className="address">Rua 17-A, 899, Edifício Centro de Medicina Amyn Daher, 2 andar, St. Aeroporto, Goiânia-GO, CEP 74070-100</p>
-        </div>
-      </div>
-      <form className="form">
 
-      </form>
-    </>
+  const resetForm = () => {
+    document.querySelector('.name-field').value = '';
+    document.querySelector('.email-field').value = '';
+    document.querySelector('.msg-field').value = '';
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Formulário enviado!');
+  }
+
+  return (
+      <div className={className} id={id}>
+        <div className="content-1">
+          <img className="logo" src={logo} />
+          <div className="social-media-icons">
+            <a href="https://www.facebook.com/draadrianamiguel/" className="social-media-link">
+              <img className="social-media-icon" src={facebookLogo}/>
+            </a>
+            <a href="https://www.instagram.com/draadrianamiguel/" className="social-media-link">
+              <img className="social-media-icon" src={instagramLogo}/>
+            </a>
+            <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5562981856722&text=Oi!" className="social-media-link">
+              <img className="social-media-icon" src={whatsappLogo}/>
+            </a>
+          </div>
+          <div className="contacts">
+            <p className="phones">(62) 3928-0988<br/>(62) 98185-6722</p>
+            <p className="address">Rua 17-A, 899, Edifício Centro de Medicina Amyn Daher, 2 andar, St. Aeroporto, Goiânia-GO, CEP 74070-100</p>
+          </div>
+        </div>
+
+
+        <form 
+          className="form email-form" 
+          method="POST"
+          action="https://formspree.io/xoqajvkj"  
+        >
+          <div className="field">
+            <label className="label">Nome</label>
+            <div className="control">
+              <input 
+                className="field name-field input is-rounded" 
+                type="text" 
+                name="name" 
+                placeholder="Nome" 
+                required
+              />
+            </div>
+          </div>
+          
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input 
+                className="field email-field input is-rounded" 
+                type="email" 
+                name="_replyto" 
+                placeholder="Seu melhor e-mail!" 
+                required
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Mensagem</label>
+            <div className="control">
+              <textarea 
+                className="field msg-field textarea is-rounded" 
+                name="message" 
+                placeholder="Digite sua mensagem" 
+                required
+              />
+            </div>
+          </div>
+          
+          <div className="field">
+            <button className="submit-btn button is-rounded is-small" type="submit">Enviar</button>
+          </div>
+
+        </form>
+      </div>
   )
 }
 
