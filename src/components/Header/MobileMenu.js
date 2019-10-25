bd31cc7800e0b7b8c8fc19253aaa80ab2162ee20
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2019-10-24 22:01:28 -0300

    Finished the mobile navbar

--- src/components/Header/MobileMenu.js
+++ src/components/Header/MobileMenu.js
@@ -1,11 +1,37 @@
 import React from 'react'
 
+import mobileLogo from './assets/mobile/mobile-logo.svg'
+import FaceLogo from './assets/mobile/facebook-mobile-logo.svg'
+import InstaLogo from './assets/mobile/instagram-mobile-logo.svg'
+import WppLogo from './assets/mobile/whatsapp-mobile-logo.svg'
+
 const MobileMenu = ({ className }) => {
   return (
-    <nav className={`${className} navbar-burger burger`}>
-      <span></span>
-      <span></span>
-      <span></span>
+    <nav className={`${className} navbar-menu mobile-menu is-fixed is-spaced is-primary`}>
+      <a href="#inicio">
+        <img className="mobile-logo navbar-brand navbar-item" src={mobileLogo} alt="" />
+      </a>
+      
+      <div className="mobile-menu-itens">
+        <div className="mobile-menu-item">
+          <a className="mobile-social-media-icon" href="http://www.facebook.com/draadrianamiguel/">
+            <img src={FaceLogo} alt=""/>
+          </a>
+        </div>
+
+        <div className="mobile-menu-item">
+          <a className="mobile-social-media-icon" href="https://www.instagram.com/draadrianamiguel/">
+            <img src={InstaLogo} alt=""/>
+          </a>
+        </div>
+        
+        <div className="mobile-menu-item">
+          <a className="mobile-social-media-icon" href="http://api.whatsapp.com/send?1=pt_BR&phone=5562981856722&text=Oi!">
+            <img src={WppLogo} alt=""/>
+          </a>
+        </div>
+      </div>
+      
     </nav>
   )
 }
